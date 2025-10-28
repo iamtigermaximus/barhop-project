920ec72a42f43ef65a59a9c5a309f611c2f55fdb
Siegfred Gamboa <siegy.gamboa@gmail.com>
2025-10-28 17:38:34 +0200

    fix: changed the prisma route in lib/auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -137,8 +137,8 @@ import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "./prisma";
 import bcrypt from "bcryptjs";
+import prisma from "./prisma";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
