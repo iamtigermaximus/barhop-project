d368904e41da1dd5ba4ed8971954dbb4348bb556
Siegfred Gamboa <siegy.gamboa@gmail.com>
2025-10-28 21:50:06 +0200

    Test: add NotificationData test page to verify enum and type

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,14 @@
-// import type { NextConfig } from "next";
-// import path from "node:path";
-
-// const nextConfig: NextConfig = {
-//   webpack: (config) => {
-//     config.resolve.alias = {
-//       ...config.resolve.alias,
-//       "@": path.resolve(__dirname, "src"), // @ → src
-//     };
-//     return config;
-//   },
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
